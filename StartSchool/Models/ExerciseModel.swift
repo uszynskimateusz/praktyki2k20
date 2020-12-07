cba2b361df6465057d6169a90d02df891a225e08
uszynskimateusz <uszynskimateusz@gmail.com>
2020-12-07 12:53:06 +0100

    JSON parsing added

---
+++ StartSchool/Models/ExerciseModel.swift
@@ -0,0 +1,22 @@
+//
+//  ExerciseModel.swift
+//  StartSchool
+//
+//  Created by Mateusz Uszyński on 06/12/2020.
+//
+
+import UIKit
+
+struct ExerciseModel {
+    let title: String
+    let description: String
+    let answer: String
+    let level: String
+    let done: Bool
+    
+    let image: UIImage
+    
+    func printAll() {
+        print("\(title), \(description), \(answer), \(level), \(done), \(image.size)")
+    }
+}
